7948023ccfd92747b5aa570963867fbf10e1b207
pragyat2611 <pragya.thakur@paxcel.net>
2020-08-01 23:35:44 +0530

    adding more testcases

--- atmMachine/app.spec.js
+++ atmMachine/app.spec.js
@@ -9,12 +9,17 @@ chai.should();
 let ATMMachine = require(path.join(__dirname,'classes/AtmMachine'));
 
 describe('Atm app', () => {
-    it('should return 500-1 \n 200-1',done => {
+    it('should return \n500-1 \n 200-2',done => {
         let w900 = new ATMMachine();
-        w900.withdrawMoney(900).should.equal({
-            '200' : 2,
-            '500' : 1
-        });
+        w900.withdrawMoney(900).should.to.deep.include({ '200': 2, '500': 1 });
+        done();
+    })
+})
+
+describe('Atm app', () => {
+    it('should return \n2000-4\n 500-3 \n 200-1 \n 100-1',done => {
+        let w9800 = new ATMMachine();
+        w9800.withdrawMoney(9800).should.to.deep.include({'100': 1, '200': 1, '500': 3 , '2000' : 4 });
         done();
     })
 })
